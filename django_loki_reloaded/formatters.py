48618661177bb4f4cc04313337dfabb4dbee94a7
h3nnn4n <uber.renan@gmail.com>
2022-07-02 18:53:05 -0300

    Isort + black cleanup

--- django_loki_reloaded/formatters.py
+++ django_loki_reloaded/formatters.py
@@ -1,24 +1,22 @@
 import logging
-import pytz
 import socket
-
 from datetime import datetime
 from logging import BASIC_FORMAT
 
+import pytz
 
-class LokiFormatter(logging.Formatter, object):
-    """
-    Python logging formatter for Loki.
-    """
-    asctime_search = '%(asctime)'
-    tz = 'UTC'
-    source = 'Loki'
-    src_host = 'localhost'
 
-    def __init__(self, fmt: str, dfmt: str, style, fqdn=False):
+class LokiFormatter(logging.Formatter):
+    asctime_search = "%(asctime)"
+    tz = "UTC"
+    source = "Loki"
+    src_host = "localhost"
+
+    def __init__(self, fmt, dfmt, style, fqdn=False):
         super(LokiFormatter, self).__init__()
+
         self.fmt = fmt or BASIC_FORMAT
-        self.dfmt = dfmt or '%Y-%m-%d %H:%M:%S'
+        self.dfmt = dfmt or "%Y-%m-%d %H:%M:%S"
         self.style = style
         if fqdn:
             self.host = socket.getfqdn()
@@ -35,35 +33,41 @@ class LokiFormatter(logging.Formatter, object):
         try:
             return self.fmt % record.__dict__
         except KeyError as e:
-            raise ValueError('Formatting field not found in record: %s' % e)
+            raise ValueError("Formatting field not found in record: %s" % e)
 
     def format(self, record):
-        # Create message dict
         record.message = record.getMessage()
+
         if self.usesTime():
             record.asctime = self.formatTime(record, self.dfmt)
+
         s = self.formatMessage(record)
+
         if record.exc_info:
             if not record.exc_text:
                 record.exc_text = self.formatException(record.exc_info)
+
         if record.exc_text:
             if s[-1:] != "\n":
                 s = s + "\n"
             s = s + record.exc_text
+
         if record.stack_info:
             if s[-1:] != "\n":
                 s = s + "\n"
+
             s = s + self.formatStack(record.stack_info)
+
         message = {
-            'streams': [
+            "streams": [
                 {
-                    'labels': f'{{source="{self.source}",job="{record.name}",host="{self.src_host}"}}',
-                    'entries': [
+                    "labels": f'{{source="{self.source}",job="{record.name}",host="{self.src_host}"}}',
+                    "entries": [
                         {
-                            'ts': self.format_timestamp(record.created).isoformat('T'),
-                            'line': s,
+                            "ts": self.format_timestamp(record.created).isoformat("T"),
+                            "line": s,
                         }
-                    ]
+                    ],
                 }
             ]
         }
